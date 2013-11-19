96eeb990c8cf1294c94ff7bb39fe2cb8c594ced1
austin1237 <austindavis4@gmail.com>
2013-11-18 18:17:39 -0700

    removed nginx frontend

--- ngnix/html/js/insert.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp', []); // Taking Angular Application in Javascript Variable
-
-myApp.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
